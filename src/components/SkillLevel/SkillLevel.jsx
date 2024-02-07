7414dc108a23a1063210e23cfb597aed29442c24
agro-pest <sathsara.psf99@gmail.com>
2024-02-07 21:14:11 +0530

    Add skills section

---
+++ src/components/SkillLevel/SkillLevel.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import './SkillLevels.css'
+
+function SkillLevel(props) {
+    const language = props.language;
+    const level = props.level;
+    let style;
+    let dotStyle;
+
+    console.log({ language, level })
+
+    switch (language) {
+        case "Java":
+            style = { backgroundColor: '#C69749', width: `${level}%` }
+            dotStyle = { backgroundColor: '#C69749' }
+            break;
+        case "Python":
+            style = { backgroundColor: '#306998', width: `${level}%` }
+            dotStyle = { backgroundColor: '#306998' }
+            break;
+        case "JavaScript":
+            style = { backgroundColor: '#FCAE2D', width: `${level}%` }
+            dotStyle = { backgroundColor: '#FCAE2D' }
+            break;
+        case "SQL":
+            style = { backgroundColor: '#00758F', width: `${level}%` }
+            dotStyle = { backgroundColor: '#00758F' }
+            break;
+        case "ReactJs":
+            style = { backgroundColor: '#7CC5D9', width: `${level}%` }
+            dotStyle = { backgroundColor: '#7CC5D9' }
+            break;
+        case "Sass":
+            style = { backgroundColor: '#CC6699', width: `${level}%` }
+            dotStyle = { backgroundColor: '#CC6699' }
+            break;
+        case "Css":
+            style = { backgroundColor: '#264DE4', width: `${level}%` }
+            dotStyle = { backgroundColor: '#264DE4' }
+            break;
+        case "Tailwind css":
+            style = { backgroundColor: '#B43297', width: `${level}%` }
+            dotStyle = { backgroundColor: '#B43297' }
+            break;
+        case "html":
+            style = { backgroundColor: '#F16529', width: `${level}%` }
+            dotStyle = { backgroundColor: '#F16529' }
+            break;
+        case "React native":
+            style = { backgroundColor: '#FCAE2D', width: `${level}%` }
+            dotStyle = { backgroundColor: '#FCAE2D' }
+            break;
+        case "Android":
+            style = { backgroundColor: '#3DDC84', width: `${level}%` }
+            dotStyle = { backgroundColor: '#3DDC84' }
+            break;
+        case "flutter":
+            style = { backgroundColor: '#41B6D0', width: `${level}%` }
+            dotStyle = { backgroundColor: '#41B6D0' }
+
+    }
+    return (
+        <div className='skill-level-container'>
+            <div className='skill-level-content'>
+                <div className='lable'>
+                    <span className='language'>{language}</span>
+                    <span className='expert-level'> {level}%</span>
+                </div>
+
+                <div className='progress'>
+                    <div className='progress-line'>
+                        <div style={style} className='level-line'>
+                            <div style={dotStyle} className='dot'> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default SkillLevel
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import './Skills.css'
+import SkillLevel from '../SkillLevel/SkillLevel'
 
 function Skills() {
 
     return (
         <div className='containers skills-container'>
-
+            <div className='skills-header'>
+                <span className='sub-header'>Empower my potential</span>
+                <span className='stroke-header '>Skills</span>
+            </div>
+            <div className='skills'>
+                <div className='skill-set-1 skill-set'>
+                    <div className='box-design skill-content-box'>
+                        <SkillLevel language='Java' level='80' />
+                        <SkillLevel language='Python' level='60' />
+                        <SkillLevel language='JavaScript' level='70' />
+                        <SkillLevel language='SQL' level='80' />
+                        <SkillLevel language='ReactJs' level='70' />
+                        <SkillLevel language='Sass' level='60' />
+                    </div>
+                </div>
+                <div className='skill-set-2 skill-set'>
+                    <div className='box-design skill-content-box'>
+                        <SkillLevel language='Css' level='80' />
+                        <SkillLevel language='Tailwind css' level='50' />
+                        <SkillLevel language='html' level='80' />
+                        <SkillLevel language='React native' level='55' />
+                        <SkillLevel language='Android' level='65' />
+                        <SkillLevel language='flutter' level='50' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
