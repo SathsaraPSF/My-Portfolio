d43e13956d44121aa766debb4ce95835c1f6028f
agro-pest <sathsara.psf99@gmail.com>
2024-02-08 22:17:08 +0530

    contact form implement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
 import Topic from '../MainTopics/Topic'
+import emailjs from '@emailjs/browser';
 import { Button } from 'react-scroll'
 function Contact() {
-    const [name, setName] = React.useState("");
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_trfv4jg', 'template_61xqlo7', form.current, {
+                publicKey: 'zE4lBLpfmyUkl7iK7',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
         <div className='containers contact-container' id='contact'>
             <div className='contact-content'>
@@ -11,22 +31,22 @@ function Contact() {
                     <Topic subHeader='Get in touch' mainHeader='Contact me' />
                 </div>
                 <div className='form'>
-                    <div className='form-content'>
+                    <form className='form-content' ref={form} onSubmit={sendEmail}>
                         <div className='details'>
                             <label className='lable'>Name</label>
-                            <input type='name' className='inputs' />
+                            <input type='name' className='inputs' name="from_name" />
                         </div>
                         <div className='details'>
                             <label className='lable'>Email</label>
-                            <input type='name' className='inputs' />
+                            <input type='name' className='inputs' name="email" />
                         </div>
                         <div className='details'>
                             <label className='lable'>Message</label>
-                            <textarea className='textarea'></textarea>
+                            <textarea className='textarea' name="message" ></textarea>
                         </div>
-                        <Button className='btn submit'>submit</Button>
+                        <input type='submit' className='btn submit' value="Send" />
 
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
