660fafd4da8471302b19bd892f775d7788bd3625
agro-pest <sathsara.psf99@gmail.com>
2024-02-08 12:33:40 +0530

    add project images and implement project section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Project.css'
 import Topic from '../MainTopics/Topic'
-
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
+import img4 from '../../assets/img4.png'
 
 function Project() {
 
@@ -14,10 +17,46 @@ function Project() {
             </div>
             <div className='projects'>
                 <div className='project-content'>
-                    <div className='project-1'></div>
-                    <div className='project-1'></div>
-                    <div className='project-1'></div>
-                    <div className='project-1'></div>
+
+                    <div className='project-1'>
+                        <div className='image'>
+                            <img src={img1} className='project-img' />
+                        </div>
+                        <div className='see-more-button'>
+                            <button className='project-button btn'>see more</button>
+                        </div>
+                    </div>
+
+                    <div className='project-1'>
+                        <div className='image'>
+                            <img src={img2} className='project-img' />
+                        </div>
+                        <div className='see-more-button'>
+                            <button className='project-button btn'>see more</button>
+                        </div>
+                    </div>
+
+                    <div className='project-1'>
+                        <div className='image'>
+                            <img src={img3} className='project-img' />
+                        </div>
+                        <div className='see-more-button'>
+                            <button className='project-button btn'>see more</button>
+                        </div>
+                    </div>
+
+                    <div className='project-1'>
+                        <div className='image'>
+                            <img src={img4} className='project-img' />
+                        </div>
+                        <div className='see-more-button'>
+                            <button className='project-button btn'>see more</button>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
             </div>
         </div>
