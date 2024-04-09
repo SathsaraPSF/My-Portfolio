1525eab89c826dee0c8e6265973fd9b15cb3b9d3
agro-pest <sathsara.psf99@gmail.com>
2024-04-09 21:09:36 +0530

    Add new project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -5,6 +5,8 @@ import img1 from '../../assets/img1.png'
 import img2 from '../../assets/img2.png'
 import img3 from '../../assets/img3.png'
 import img4 from '../../assets/img4.png'
+import img5 from '../../assets/img5.png'
+import img6 from '../../assets/img6.png'
 import ProjectCard from './ProjectCard'
 
 function Project() {
@@ -19,6 +21,8 @@ function Project() {
             <div className='projects'>
                 <div className='project-content'>
                     <ProjectCard img={img3} link="https://github.com/SathsaraPSF/My-Portfolio" />
+                    <ProjectCard img={img5} link="https://github.com/SathsaraPSF/AT-DIGITAL-FRONTEND" />
+                    <ProjectCard img={img6} link="https://github.com/SathsaraPSF/Simple-note-taking-application--frontend" />
                     <ProjectCard img={img1} link="https://github.com/SathsaraPSF/MyToDoList" />
                     <ProjectCard img={img2} link="https://github.com/SathsaraPSF/SPCreations-Web-Site" />
                     <ProjectCard img={img4} link="https://github.com/Gold-Diggers-Team/AgroPest-Mobile-Application" />
